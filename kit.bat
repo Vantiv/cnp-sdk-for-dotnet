b4e12e7151f035e9c18115a83559793140f144aa
Chahat, Sharma <chahat.sharma@vantiv.com>
2017-12-04 13:55:04 -0500

    changing names of dll and config files in kit.bat from litle to cnp

--- kit.bat
+++ kit.bat
@@ -3,14 +3,14 @@ IF "%1"==[] (
 	exit /b
 )
 
-copy LitleSdkForNet\LitleSdkForNet\bin\Release\LitleSdkForNet.dll .\
-copy LitleSdkForNet\LitleSdkForNet\bin\Release\LitleSdkForNet.dll.config .\
-copy LitleSdkForNet\LitleSdkForNet\bin\Release\DiffieHellman.dll .\
-copy LitleSdkForNet\LitleSdkForNet\bin\Release\Tamir.SharpSSH.dll .\
-copy LitleSdkForNet\LitleSdkForNet\bin\Release\Org.Mentalis.Security.dll .\
-"C:\Program Files\7-Zip\7z.exe" a LitleSdkForNet-%1.zip CHANGELOG LICENSE LitleSdkForNet.dll LitleSdkForNet.dll.config README.md DiffieHellman.dll Tamir.SharpSSH.dll Org.Mentalis.Security.dll
-del LitleSdkForNet.dll
-del LitleSdkForNet.dll.config
+copy CnpSdkForNet\CnpSdkForNet\bin\Release\CnpSdkForNet.dll .\
+copy CnpSdkForNet\CnpSdkForNet\bin\Release\CnpSdkForNet.dll.config .\
+copy CnpSdkForNet\CnpSdkForNet\bin\Release\DiffieHellman.dll .\
+copy CnpSdkForNet\CnpSdkForNet\bin\Release\Tamir.SharpSSH.dll .\
+copy CnpSdkForNet\CnpSdkForNet\bin\Release\Org.Mentalis.Security.dll .\
+"C:\Program Files\7-Zip\7z.exe" a CnpSdkForNet-%1.zip CHANGELOG LICENSE CnpSdkForNet.dll CnpSdkForNet.dll.config README.md DiffieHellman.dll Tamir.SharpSSH.dll Org.Mentalis.Security.dll
+del CnpSdkForNet.dll
+del CnpSdkForNet.dll.config
 del DiffieHellman.dll
 del Tamir.SharpSSH.dll
 del Org.Mentalis.Security.dll
