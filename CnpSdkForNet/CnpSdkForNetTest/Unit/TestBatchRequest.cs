a7c6451117bdc60f4e42348ef7a8fba6c33db4a2
Wendy Wu <Wendy.Wu@vantiv.com>
2019-08-22 11:52:30 -0400

    Fixed version to be 12.8.2

--- CnpSdkForNet/CnpSdkForNetTest/Unit/TestBatchRequest.cs
+++ CnpSdkForNet/CnpSdkForNetTest/Unit/TestBatchRequest.cs
@@ -55,7 +55,7 @@ namespace Cnp.Sdk.Test.Unit
             var actual = batchRequest.generateXmlHeader();
             var expected = @"
 <batchRequest id=""""
-merchantSdk=""DotNet;12.8.1""
+merchantSdk=""DotNet;12.8.2""
 merchantId=""01234"">
 ";
             
