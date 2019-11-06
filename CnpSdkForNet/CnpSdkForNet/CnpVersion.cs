9237a6966ad760b25b3314286546981edbe3d12e
Zachary Cook <35985322+zxc8027@users.noreply.github.com>
2019-11-06 10:50:54 -0500

    Incremented version number

--- CnpSdkForNet/CnpSdkForNet/CnpVersion.cs
+++ CnpSdkForNet/CnpSdkForNet/CnpVersion.cs
@@ -9,6 +9,6 @@ namespace Cnp.Sdk
     public class CnpVersion
     {
         public const String CurrentCNPXMLVersion = "12.9";
-        public const String CurrentCNPSDKVersion = "12.9.0";
+        public const String CurrentCNPSDKVersion = "12.9.1";
     }
 }
