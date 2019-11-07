c70bdc7bf223956583fe64b6aecfc80a45965f0a
zxc8027 <zxc8027@g.rit.edu>
2019-11-07 14:16:36 -0500

    Added file with maim method for compiling

---
+++ CnpSdkForNet/CnpSdkForNetTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cnp.Sdk.Test
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
