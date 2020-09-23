76cca6b8914fb197f5fa9eb5039ecc82ca45b0a7
Gregory Conrad <gsconrad@wpi.edu>
2020-09-23 14:15:34 -0400

    Updated versioning information

--- CnpSdkForNet/CnpSdkForNet/CnpVersion.cs
+++ CnpSdkForNet/CnpSdkForNet/CnpVersion.cs
@@ -9,6 +9,6 @@ namespace Cnp.Sdk
     public class CnpVersion
     {
         public const String CurrentCNPXMLVersion = "12.14";
-        public const String CurrentCNPSDKVersion = "12.14.0";
+        public const String CurrentCNPSDKVersion = "12.14.1";
     }
 }
