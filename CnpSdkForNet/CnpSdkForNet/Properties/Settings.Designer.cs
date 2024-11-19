cd08966fb175e0f986785f96ce42c4c1889b08b2
e5651806 <sushamaghadage38@gmail.com>
2024-11-19 10:18:10 +0530

    Promoting changes for 12.40.2

--- CnpSdkForNet/CnpSdkForNet/Properties/Settings.Designer.cs
+++ CnpSdkForNet/CnpSdkForNet/Properties/Settings.Designer.cs
@@ -313,7 +313,7 @@ namespace Cnp.Sdk.Properties {
 
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        [global::System.Configuration.DefaultSettingValueAttribute("false")]
         public string encryptOltpPayload
         {
             get
